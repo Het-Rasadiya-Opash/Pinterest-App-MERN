559287c00e1eb2e53f566abe3f1865733061a73e
Het <hra.opash@gmail.com>
2026-02-18 18:18:36 +0530

    .

--- client/src/routes/postPage/PostPage.jsx
+++ client/src/routes/postPage/PostPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./PostPage.css";
+import "./postPage.css";
 import Image from "../../components/image/Image";
 import PostInteractions from "../../components/postInteractions/PostInteractions";
 import { Link, useParams } from "react-router-dom";
