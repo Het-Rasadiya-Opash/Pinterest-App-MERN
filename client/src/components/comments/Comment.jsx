67bfaf4ce749fe1d93d1125553a30e7dc9d4e706
Het <hra.opash@gmail.com>
2026-02-24 13:36:07 +0530

    .

--- client/src/components/comments/Comment.jsx
+++ client/src/components/comments/Comment.jsx
@@ -4,7 +4,7 @@ import {format} from "timeago.js"
 const Comment = ({ comment }) => {
   return (
     <div className="flex gap-4 max-md:gap-3">
-      <Image path={comment.user.img || "/general/noAvatar.pngP"} alt="" className="w-8 h-8 rounded-full object-cover max-md:w-7 max-md:h-7" />
+      <Image path={comment.user.img || "/general/noAvatar.png"} alt="" className="w-8 h-8 rounded-full object-cover max-md:w-7 max-md:h-7" />
       <div className="flex flex-col gap-1 flex-1">
         <span className="font-medium text-sm">{comment.user.displayName}</span>
         <p className="text-sm wrap-break-word">
